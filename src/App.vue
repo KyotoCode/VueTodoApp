e76e4c6d545be76308dbc4d0b7710e520f66d7df
Mihajlo <milosevicmihajlo@gmail.com>
2020-08-09 18:27:27 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <h1>ToDoApp</h1>
+      <input
+        class="u-full-width"
+        type="text"
+        v-model="todoText"
+        placeholder="Enter todo"
+        id="inputDos"
+      />
+      <button class="button-primary" @click="btnSub" id="submitBtn">Button element</button>
+    </main>
+    <hr />
+    <section>
+      <ul>
+        <li v-for="(todo, i) in existingTodo" :key="(todo.name, i)">
+          <span>{{i + 1}}: {{todo.name}}</span>
+          <button @click="btnDelete(i)">Remove</button>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      todoText: "",
+      existingTodo: [
+        { name: "go shoping", id: 1 },
+        { name: "go to school", id: 2 },
+      ],
+    };
+  },
+  methods: {
+    btnSub() {
+      this.existingTodo.push({
+        name: this.todoText,
+      });
+    },
+    btnDelete(i) {
+      this.existingTodo.splice(i, 1);
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +56,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#inputDos {
+  text-align: center;
+  width: 50%;
+}
+#todos {
+  display: flex;
+  justify-content: center;
+}
+#todos button {
+  margin-left: 50px;
+}
+hr {
+  width: 80%;
+}
+ul button {
+  margin-left: 50px;
 }
 </style>
